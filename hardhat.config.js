e390062f36916fe961cd7e1f7138dc1d97c9783b
Ashish Kumar <ashish.kumar@zopper.com>
2024-06-18 12:37:39 +0530

    deployment changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
+console.log({ API_URL, PRIVATE_KEY });
 module.exports = {
   solidity: "0.8.26",
-  networks:{
-    hardhat:{
-      chainId:1337,
-    }
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
   },
-  paths:{
-    artifacts:"./client/src/artifacts",
-  }
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
 };
